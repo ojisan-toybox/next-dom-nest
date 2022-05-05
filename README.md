e3db97dbb637d02618d463c30784590e33cfd6dd
sadness_ojisan <sadness.ojisan@gmail.com>
2022-05-06 04:36:56 +0900

    add app

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # next-dom-nest
+
 NextJS でどういう DOM のネストができないか実験する
+
+React のバージョンが v17 なら動く。これを v18 にあげて、`next dev`すると動かない
---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+export default () => {
+  return (
+    <div>
+      <p>
+        p tag
+        <div>div</div>
+      </p>
+    </div>
+  );
+};
